aa470ace6c6415ca6f445c30c6317ee035d454d3
pavellenivtsev <60518554+pavellenivtsev@users.noreply.github.com>
2020-04-13 20:08:24 +0300

    delete trash

--- javaschool/src/main/java/com/tsystems/dao/impl/WaypointDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.tsystems.dao.impl;
-
-import com.tsystems.dao.api.WaypointDao;
-import com.tsystems.entity.Waypoint;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class WaypointDaoImpl extends AbstractGenericDao<Waypoint> implements WaypointDao {
-}
